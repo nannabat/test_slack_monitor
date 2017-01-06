82fb53fbf58ebc8a156aca492b6ed076eaddf0e3
Narendra Annabathula <narendra.33@gmail.com>
2017-01-05 22:59:38 -0800

    able to send text and buttons but no response to buttons

--- sample_python.py
+++ sample_python.py
@@ -3,13 +3,48 @@ import os
 from slackclient import SlackClient
 
 #slack_token = os.environ["xoxb-121355730919-9IEodFmVZVwsgOV4GP7yh4F7"]
-sc = SlackClient("xoxb-121355730919-9IEodFmVZVwsgOV4GP7yh4F7")
+sc = SlackClient("xoxb-121355730919-8oFqlfd751dfxhK0j9Q9Rax8")
+attachments_var = [{
+            "text": "Choose a game to play",
+            "fallback": "You are unable to choose a game",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "chess",
+                    "text": "Chess",
+                    "type": "button",
+                    "value": "chess"
+                },
+                {
+                    "name": "maze",
+                    "text": "Falken's Maze",
+                    "type": "button",
+                    "value": "maze"
+                },
+                {
+                    "name": "war",
+                    "text": "Thermonuclear War",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "war",
+                    "confirm": {
+                        "title": "Are you sure?",
+                        "text": "Wouldn't you prefer a good game of chess?",
+                        "ok_text": "Yes",
+                        "dismiss_text": "No"
+                    }
+                }
+            ]
+        }]
 
 response = sc.api_call(
   "chat.postMessage",
   channel="#oncall_alerts",
   #text="Hello from Python! :tada:"
   text = "This is my perfect message",
+  attachments = attachments_var,
   username = 'ops_bot',
   as_user = "true"
 )
