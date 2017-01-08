d3dcdf2691cba2e083b18804bfc5704b7c7743eb
Narendra Annabathula <narendra.33@gmail.com>
2017-01-08 11:30:44 -0800

    working slack_bot_alert as functions

--- slack_bot_alert.py
+++ slack_bot_alert.py
@@ -8,60 +8,79 @@ def get_slack_token():
    return tkn
 
 
-token = get_slack_token()
-   
 
-#slack_token = os.environ["xoxb-121355730919-9IEodFmVZVwsgOV4GP7yh4F7"]
-sc = SlackClient(token)
-attachments_var = [{
-            "text": "Choose a game to play",
-            "fallback": "You are unable to choose a game",
-            "callback_id": "wopr_game",
-            "color": "#3AA3E3",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "chess",
-                    "text": "Chess",
-                    "type": "button",
-                    "value": "chess"
-                },
-                {
-                    "name": "maze",
-                    "text": "Falken's Maze",
-                    "type": "button",
-                    "value": "maze"
-                },
-                {
-                    "name": "war",
-                    "text": "Thermonuclear War",
-                    "style": "danger",
-                    "type": "button",
-                    "value": "war",
-                    "confirm": {
-                        "title": "Are you sure?",
-                        "text": "Wouldn't you prefer a good game of chess?",
-                        "ok_text": "Yes",
-                        "dismiss_text": "No"
+def send_alert_to_slack():
+    token = get_slack_token()
+       
+
+    #slack_token = os.environ["xoxb-121355730919-9IEodFmVZVwsgOV4GP7yh4F7"]
+    sc = SlackClient(token)
+    # attachments_var = [{
+    #             "text": "Choose a game to play",
+    #             "fallback": "You are unable to choose a game",
+    #             "callback_id": "wopr_game",
+    #             "color": "#3AA3E3",
+    #             "attachment_type": "default",
+    #             "actions": [
+    #                 {
+    #                     "name": "chess",
+    #                     "text": "Chess",
+    #                     "type": "button",
+    #                     "value": "chess"
+    #                 },
+    #                 {
+    #                     "name": "maze",
+    #                     "text": "Falken's Maze",
+    #                     "type": "button",
+    #                     "value": "maze"
+    #                 },
+    #                 {
+    #                     "name": "war",
+    #                     "text": "Thermonuclear War",
+    #                     "style": "danger",
+    #                     "type": "button",
+    #                     "value": "war",
+    #                     "confirm": {
+    #                         "title": "Are you sure?",
+    #                         "text": "Wouldn't you prefer a good game of chess?",
+    #                         "ok_text": "Yes",
+    #                         "dismiss_text": "No"
+    #                     }
+    #                 }
+    #             ]
+    #         }]
+    attachments_var =     [{
+ 
+                "title": "Values of on file httpd.conf have changed on al1_prod_hadoop1",
+                "text": "Values changed are ",
+                "fields": [
+                    {
+                        "title": "Priority",
+                        "value": "High",
+                        "short": "false"
                     }
-                }
-            ]
-        }]
+                ],
+                "image_url": "http://my-website.com/path/to/image.jpg",
+                "thumb_url": "http://example.com/path/to/thumb.png",
+                "footer": "Powering Scientific Discovery Since 1974",
+                "footer_icon": "http://www.nersc.gov/assets/Logos/_resampled/CroppedResize130130-NERSClogoslatepmssolid-sm.png",
+                "ts": 123456789
+            }]
 
-response = sc.api_call(
-  "chat.postMessage",
-  channel="#oncall_alerts",
-  #text="Hello from Python! :tada:"
-  text = "This is message is from reading token from file",
-  attachments = attachments_var,
-  username = 'ops_bot',
-  as_user = "true"
-)
+    response = sc.api_call(
+      "chat.postMessage",
+      channel="#oncall_alerts",
+      #text="Hello from Python! :tada:"
+      text = "File monitoring alert, ticket INC230134 is created",
+      attachments = attachments_var,
+      username = 'ops_bot',
+      as_user = "true"
+    )
 
-print response['ok']
-message_delivered = response['ok']
+    print response['ok']
+    message_delivered = response['ok']
 
-if message_delivered == True:
-  print "message delivered"
+    if message_delivered == True:
+      print "message delivered"
 
-print response
+    print response
