8227a9728ec75db0810cd3f7e4522a97558a44c9
Narendra Annabathula <narendra.33@gmail.com>
2017-01-08 16:23:19 -0800

    added tkt number and service now link

--- slack_bot_alert.py
+++ slack_bot_alert.py
@@ -9,7 +9,7 @@ def get_slack_token():
 
 
 
-def send_alert_to_slack(var_title,details):
+def send_alert_to_slack(var_title,details,tkt_number):
     token = get_slack_token()
        
 
@@ -53,6 +53,9 @@ def send_alert_to_slack(var_title,details):
  
                 "title": var_title,
                 "text": details,
+                "author_name": tkt_number
+                "author_link": "https://dev27484.service-now.com//nav_to.do?uri=incident.do?sysparm_query=number="+tkt_number
+
                 "fields": [
                     {
                         "title": "Priority",
@@ -63,14 +66,15 @@ def send_alert_to_slack(var_title,details):
                 "image_url": "http://my-website.com/path/to/image.jpg",
                 "thumb_url": "http://example.com/path/to/thumb.png",
                 "footer": "Powering Scientific Discovery Since 1974",
-                "footer_icon": "http://www.nersc.gov/assets/Logos/_resampled/CroppedResize130130-NERSClogoslatepmssolid-sm.png"
+                "footer_icon": "http://www.nersc.gov/assets/Logos/_resampled/CroppedResize130130-NERSClogoslatepmssolid-sm.png"0
             }]
 
     response = sc.api_call(
       "chat.postMessage",
       channel="#oncall_alerts",
       #text="Hello from Python! :tada:"
-      text = "File monitoring alert, ticket INC230134 is created",
+      #text = 'File monitoring alert,ticket ' + TKT_NUMBER + 'is created',
+      text = 'File monitoring alert,ticket ' + TKT_NUMBER + 'is created'
       attachments = attachments_var,
       username = 'ops_bot',
       as_user = "true"
